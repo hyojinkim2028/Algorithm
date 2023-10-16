0eb2ee0b8c461d4467da6b72e945ce4b3f4b3ff3
rizzy <12hyojinkim14@gmail.com>
2023-10-16 12:24:15 +0900

    퀵정렬

--- 자료구조&알고리즘/4_퀵정렬.js
+++ 자료구조&알고리즘/4_퀵정렬.js
@@ -6,15 +6,19 @@ function quicSort(arr) {
   let left = []
   let rigth = []
 
+  // 피봇값+1 번째부터 시작
   for (let i = 1; i < answer.length; i++) {
-    // 피봇값+1 번째부터 시작
+    // 값이 피봇값보다 작거나 같으면 왼쪽에
     if (arr[i] <= pivot) left.push(arr[i])
+    // 크면 오른쪽에 넣음
     else rigth.push(arr[i])
   }
 
+  // 재귀함수 -> 더 자를수 없을때까지 함수 안에 함수를 돌림
   let leftSort = quicSort(left)
   let rigthSort = quicSort(rigth)
 
+  // 전개구문으로 한 배열안에 넣어 리턴
   answer = [...leftSort, pivot, ...rigthSort]
   return answer
 }
